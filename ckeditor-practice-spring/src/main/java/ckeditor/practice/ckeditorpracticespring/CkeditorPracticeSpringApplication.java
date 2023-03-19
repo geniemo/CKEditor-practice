f984ba2aaa8574cd2338d2f8bd803943652aeba8
geniemo <kor98won@gmail.com>
2023-03-19 14:28:58 +0900

    init: 프로젝트 생성

---
+++ ckeditor-practice-spring/src/main/java/ckeditor/practice/ckeditorpracticespring/CkeditorPracticeSpringApplication.java
@@ -0,0 +1,13 @@
+package ckeditor.practice.ckeditorpracticespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkeditorPracticeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkeditorPracticeSpringApplication.class, args);
+	}
+
+}
---
+++ ckeditor-practice-spring/src/test/java/ckeditor/practice/ckeditorpracticespring/CkeditorPracticeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ckeditor.practice.ckeditorpracticespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkeditorPracticeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
