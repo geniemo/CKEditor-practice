e78a5ffd2054690830bd809e062dd05c3ce65406
geniemo <kor98won@gmail.com>
2023-03-20 02:30:09 +0900

    feat: 게시글 업로드 기능

--- ckeditor-practice-react/src/components/Editor.js
+++ ckeditor-practice-react/src/components/Editor.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import MyButton from "./MyButton";
 
 const Editor = () => {
+  const navigate = useNavigate();
+
+  const titleRef = useRef();
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
   const customUploadAdapter = (loader) => {
     return {
       upload() {
@@ -32,11 +41,39 @@ const Editor = () => {
     };
   }
 
+  const handleSubmit = () => {
+    if (title.length < 1) {
+      titleRef.current.focus();
+      return;
+    }
+
+    const data = {
+      title,
+      content,
+    };
+
+    axios.post("http://localhost:8080/api/v0/post", data).then((res) => {
+      if (res.status === 200) {
+        navigate("/", { replace: true });
+        return;
+      } else {
+        alert("업로드 실패.");
+        return;
+      }
+    });
+  };
+
   return (
     <div className="Editor">
       <section>
         <div className="title-wrapper">
-          <textarea className="input-title" placeholder="제목을 입력하세요" />
+          <textarea
+            className="input-title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="제목을 입력하세요"
+            ref={titleRef}
+          />
         </div>
       </section>
       <section>
@@ -49,8 +86,8 @@ const Editor = () => {
             console.log("Editor is ready to use!", editor);
           }}
           onChange={(event, editor) => {
-            const data = editor.getData();
-            console.log({ event, editor, data });
+            setContent(editor.getData());
+            console.log({ event, editor, content });
           }}
           onBlur={(event, editor) => {
             console.log("Blur.", editor);
@@ -59,6 +96,19 @@ const Editor = () => {
             console.log("Focus.", editor);
           }}
         />
+      </section>
+      <section>
+        <div className="control-box">
+          <div className="cancel-btn-wrapper">
+            <MyButton
+              text={"취소"}
+              onClick={() => navigate(-1, { replace: true })}
+            />
+          </div>
+          <div className="submit-btn-wrapper">
+            <MyButton text={"완료"} type="black" onClick={handleSubmit} />
+          </div>
+        </div>
       </section>
     </div>
   );
